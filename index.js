b7ee96a0549b1989f32120118d15c343dfa64902
Arthur Ruiz <23.01445-8@maua.br>
2026-04-16 20:30:04 -0300

    test(comandos): testa o reconhecimento dos comandos add e list

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 const args = process.argv
 
 //separando em comando e título da tarefa
-const comando = args[2]
+const comando = args[2].toLowerCase().trim()        
 const titulo = args[3]
+
+//opções para cada comando
+switch (comando) {
+    case "add":
+        console.log("Adicionando tarefas")
+        break;
+    case "list":
+        console.log("Listando tarefas")
+        break;
+
+    default:
+        console.log("Comando inválido")
+        break;
+}
