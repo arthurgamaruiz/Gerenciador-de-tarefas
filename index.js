fba5d386df3daa874b06f3750492acf8e2613ad8
Arthur Ruiz <23.01445-8@maua.br>
2026-04-17 00:00:33 -0300

    feat(salva-tarefas): implementa json que salva as tarefas

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 //argumentos da entrada via terminal 
 const args = process.argv
 
-//separando em comando e título da tarefa
-const comando = args[2].toLowerCase().trim()        
-const titulo = args[3]
+//leitura do arquvio 
+const fs = require("fs")
+const dados = fs.readFileSync('tarefas.json')       //lendo arquivo com as tarefas
 
-//lista que armazena as tarefas 
-const tarefas = []
+//converter para array
+const tarefas = JSON.parse(dados)
+
+//separando em comando e título da tarefa
+//formato: comando tarefa id status
+//JSON
+const comando = args[2]
+const tarefa = {
+    id: tarefas.length+1, 
+    titulo: args[3], 
+    status: args[4]
+}
 
 //opções para cada comando
 switch (comando) {
     case "add":
-        tarefas.push(titulo)            //adiciona ao final da lista
-        console.log(tarefas)
+        tarefas.push(tarefa)
+        const jsonString = JSON.stringify(tarefas, null, 2)   
+        fs.writeFileSync('tarefas.json', jsonString)            //salva tudo de volta no arquivo
+        console.log("Tarefa adicionada!")
         break;
     case "list":
-        console.log("Listando tarefas")
         break;
-
+    case "remove":
+        console.log("Removendo tarefas")
+        break;
     default:
         console.log("Comando inválido")
         break;
